d6d491052a06b6cffbe2434f3e290374fe934dd2
Unknown <mr.simon3500@gmail.com>
2018-08-25 21:54:22 +0200

    It works. It's just not completely accurate at 4th and 5th decimal place

--- Java Version/src/com/company/BruteforceCalculator.java
+++ Java Version/src/com/company/BruteforceCalculator.java
@@ -3,24 +3,27 @@ package com.company;
 import com.sun.jdi.request.DuplicateRequestException;
 import jdk.nashorn.api.tree.ForInLoopTree;
 
+import javax.sound.midi.Soundbank;
 import java.util.Random;
 import java.util.function.DoubleBinaryOperator;
 
 public class BruteforceCalculator
 {
-	private double constantC;
+	private double constantC = 0;
 	private double returnC = 0;
 	private Random random = new Random();
-	private final int ITERATIONS = 500_000;      //How many times to check for a given event. Bigger = slower, but more accurate
+	private final int ITERATIONS = 200_000;      //How many times to check for a given event. Bigger = slower, but more accurate
 	private final int GRANULARITY = 100;        //How little to change C each iteration. Higher = smaller changes.
-	private final double TOLERANCE = 10E-9;     //Tolerance for the chance being returned.
-	private final int REPEATS = 10;             //How many times to repeat the process. More repeats = slower, but more accurate.
+	private final double TOLERANCE = 10E-15;     //Tolerance for the chance being returned.
+	private final int REPEATS = 25;             //How many times to repeat the process. Doesn't
 
 	public double CalculateC(double target)
 	{
 		double hitCounter;
 		int N = 1;
 		double resultChance = 0;
+		constantC = 0;
+		returnC = 0;
 
 		if (target == 1.0 || target == 0)
 			return target;
@@ -28,28 +31,30 @@ public class BruteforceCalculator
 		long startTime = System.currentTimeMillis();
 		for (int i = 0; i < REPEATS; i++)
 		{
+			long innerStart = System.currentTimeMillis();
 			while (Math.abs(resultChance - target) > TOLERANCE)
 			{
 				constantC -= (resultChance - target) / GRANULARITY;
 				hitCounter = 0;
 				for (int j = 0; j < ITERATIONS; j++)
-				{
-					if (random.nextDouble() < constantC * N)
+					if (random.nextFloat() < constantC * N)
 					{
 						hitCounter += 1.0;
 						N = 1;
-					} else
+					}
+					else
 						N++;
-				}
 				resultChance = hitCounter / ITERATIONS;
 			}
 			returnC += constantC;
-			constantC = random.nextDouble();
-			resultChance = 0;
+			resultChance = target-constantC;
+			constantC = target;
+			double innerDuration = (System.currentTimeMillis() - innerStart);
+			System.out.println("Loop " + (i+1) + " out of " + REPEATS + " done. Time taken: " + innerDuration/1000 + " seconds.");
 		}
 		long endTime = System.currentTimeMillis();
 		double duration = (endTime - startTime);
-		System.out.println("Done calculating! Sorry for the wait. Time taken: " + duration/1000 + " seconds");
+		System.out.println("Done calculating! Sorry for the wait. Time taken: " + duration/1000 + " seconds.");
 		return returnC/REPEATS;
 	}
 }
--- Java Version/src/com/company/Main.java
+++ Java Version/src/com/company/Main.java
@@ -1,27 +1,48 @@
 package com.company;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.Scanner;
-import java.util.concurrent.PriorityBlockingQueue;
 
 public class Main {
 
     public static void main(String[] args)
     {
         double ConstantC = 0;
-//        Calculator calculator = new Calculator();
-//        WildCalculator wildCalculator = new WildCalculator();
         BruteforceCalculator calculator = new BruteforceCalculator();
         Scanner scanner = new Scanner(System.in);
+        String inputString;
+        double desiredChance = 0;
+        DecimalFormat df = new DecimalFormat("0.00000");
+        df.setRoundingMode(RoundingMode.DOWN);
 
-        System.out.print("Welcome! Enter your desired percentage chance (without %): ");
-        double desiredChance = scanner.nextDouble();
-        //TODO: Add checks on bounds
-        System.out.println("Thank you. Calculating...");
-
-//        ConstantC = calculator.CalculateC(desiredChance);
-        ConstantC = calculator.CalculateC(desiredChance/100);
-
-        System.out.println("For your desired chance of " + desiredChance + "% use the (approximate) C value: " + ConstantC);
+        System.out.println("Welcome! Enter your desired percentage chance (without %). Alternatively, enter \"exit\" to close the program.");
+        do
+        {
+            inputString = scanner.nextLine();
+            //TODO: Add checks on bounds
+            if (isNumeric(inputString))
+            {
+                System.out.println("Thank you. Calculating...");
+                desiredChance = Double.parseDouble(inputString);
+                ConstantC = calculator.CalculateC(desiredChance / 100);
+                System.out.println("For your desired chance of " + desiredChance + "% use the (approximate) C value: " + df.format(ConstantC));
+                System.out.println("Enter a new number to recalculate, or \"exit\" to exit.");
+            }
+        } while (!inputString.toLowerCase().equals("exit"));
         System.out.println("Exiting");
+    }
+
+    public static boolean isNumeric(String str)
+    {
+        try
+        {
+            double d = Double.parseDouble(str);
+        }
+        catch(NumberFormatException nfe)
+        {
+            return false;
+        }
+        return true;
     }
 }
